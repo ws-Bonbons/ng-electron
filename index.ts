961849798cf2211d64a8fbf8c2e368c61bd32a33
Big Mogician <sunaoran@youzan.com>
2019-06-13 16:58:03 +0800

    add gulp

---
+++ index.ts
@@ -0,0 +1,5 @@
+import * as cmd from "commander";
+
+cmd.name("ng-electron");
+cmd.description("ng-electron description.");
+cmd.option("-C, --create [projectName]", "create a new ng-electron project.");
